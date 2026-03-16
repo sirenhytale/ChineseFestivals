bc7cd7a36d72544aea8d7cdf25bac1a6daac9038
meFroggy <SubToFroggy@gmail.com>
2026-03-15 22:42:31 -0400

    Update Check on Startup

--- src/main/java/plugin/siren/ChineseFestivals.java
+++ src/main/java/plugin/siren/ChineseFestivals.java
@@ -2,13 +2,16 @@ package plugin.siren;
 
 import com.hypixel.hytale.event.EventRegistration;
 import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.server.core.HytaleServer;
 import com.hypixel.hytale.server.core.event.events.player.PlayerReadyEvent;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 import plugin.siren.Events.PlayerReadyEventCF;
 import plugin.siren.Utils.HStats;
+import plugin.siren.Utils.UpdateChecker;
 
 import javax.annotation.Nonnull;
+import java.util.concurrent.TimeUnit;
 
 public class ChineseFestivals extends JavaPlugin {
     private static final String VERSION = "0.1.1";
@@ -38,9 +41,25 @@ public class ChineseFestivals extends JavaPlugin {
         }
 
         LOGGER.atInfo().log("Version " + VERSION + " of Chinese Festivals has successfully loaded.");
+
         if(ifDebug()){
             LOGGER.atInfo().log("= =- -=- -=- -=- -=- -=- -=- -=- -=- -=- -= =");
             LOGGER.atInfo().log("Loaded Chinese Festivals in Debug mode.");
+        }
+
+        String recentVersion = UpdateChecker.checkForUpdate();
+        if(!ChineseFestivals.getVersion().equalsIgnoreCase(recentVersion)){
+            LOGGER.atInfo().log("= =- -=- -=- -=- -=- -=- -=- -=- -=- -=- -= =");
+            String versionMessage = "The Chinese Festivals Mod version is outdated, Chinese Festivals has released v" + recentVersion +".";
+            ChineseFestivals.LOGGER.atInfo().log(versionMessage);
+
+            Runnable updateCheckRunnable = new Runnable() {
+                @Override
+                public void run() {
+                    LOGGER.atInfo().log(versionMessage);
+                }
+            };
+            HytaleServer.SCHEDULED_EXECUTOR.schedule(updateCheckRunnable,15, TimeUnit.SECONDS);
         }
         LOGGER.atInfo().log("===---==---==---==---==---==---==---==---==---==---===");
     }
